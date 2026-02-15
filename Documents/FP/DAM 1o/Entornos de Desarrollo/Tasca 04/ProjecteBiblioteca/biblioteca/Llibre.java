7f0180ecf858a9723b466026c7bb7fdf6352da25
unfho <unfho@DESKTOP-HBMDIN4>
2026-02-15 21:38:43 +0100

    Añadidos comentarios JavaDoc

--- Documents/FP/DAM 1o/Entornos de Desarrollo/Tasca 04/ProjecteBiblioteca/biblioteca/Llibre.java
+++ Documents/FP/DAM 1o/Entornos de Desarrollo/Tasca 04/ProjecteBiblioteca/biblioteca/Llibre.java
@@ -1,43 +1,92 @@
 package biblioteca;
 
+/**
+ * Clase Llibre y métodos para manipularla
+ * 
+ * @author - Francisco José Suárez Muñoz
+ */
 public class Llibre {
+	/** Título del libro */
     private String titol;
+    /** Autor del libro */
     private String autor;
+    /** Cuántos ejemplares del libro tenemos */
     private int exemplars;
+    /** Precio del libro */
     private float preu;
 
+    /**
+     *  Devuelve el precio del libro
+     *  @return precio del libro 
+     */
     float getPreu() {
 		return preu;
 	}
 
+    /** 
+     * Modifica el precio del libro
+     * @param preu precio del libro
+     */
 	void setPreu(float preu) {
 		this.preu = preu;
 	}
 
+	/** 
+	 * Devuelve el número de ejemplares del libro
+	 * @return número de ejemplares del libro
+	 */
 	int getExemplars() {
 		return exemplars;
 	}
 
+	/** 
+	 * Modifica el número de ejemplares del libro
+	 * @param exemplars número de ejemplares del libro
+	 */
 	void setExemplars(int exemplars) {
 		this.exemplars = exemplars;
 	}
 
+	/** 
+	 * Devuelve el autor del libro
+	 * @return autor del libro
+	 */
 	String getAutor() {
 		return autor;
 	}
 
+	/** 
+	 * Modifica el nombre del autor del libro
+	 * @param autor autor del libro
+	 */
 	void setAutor(String autor) {
 		this.autor = autor;
 	}
 
+	/** 
+	 * Devuelve el título del libro
+	 * @return título del libro
+	 */
 	String getTitol() {
 		return titol;
 	}
 
+	/** 
+	 * Modifica el título del libro
+	 * @param titol título del libro */
 	void setTitol(String titol) {
 		this.titol = titol;
 	}
 
+	/** 
+	 * Crea un libro e inicia todos los miembros
+	 * 
+	 * @param titol título del libro
+	 * @param autor autor del libro
+	 * @param exemplars número de ejemplares del libro
+	 * @param preu precio del libro
+	 * 
+	 */
 	public Llibre(String titol, String autor, int exemplars, float preu) {
         this.setTitol(titol);
         this.setAutor(autor);
@@ -45,6 +94,12 @@ public class Llibre {
         this.setPreu(preu);
     }
 
+	/** 
+	 * Realiza el préstamo de una cantidad determinada de libros
+	 *  Si hay suficiente ejemplares para realizar el préstamo, actualiza el total de ejemplares
+	 *  @param quantitat número de ejemplares a prestar
+	 *  @throws Exception Si la cantidad de ejemplares solicitada es mayor a las que tenemos disponibles
+	 */
     public void prestarLlibre(int quantitat) throws Exception {
         if (getExemplars() >= quantitat) {
             setExemplars(getExemplars() - quantitat);
@@ -54,11 +109,18 @@ public class Llibre {
         }
     }
 
+    /** 
+     * Devuelve una cantidad determinada de libros
+     *  @param quantitat cantidad de libros a devolver
+     */
     public void retornarLlibre(int quantitat) {
         setExemplars(getExemplars() + quantitat);
         System.out.println("S'han retornat " + quantitat + " exemplars.");
     }
 
+    /**
+     *  Imprime por consola el título, autor, número de ejemplares y precio del libro
+     */
     public void mostrarDades() {
         System.out.println("Títol: " + getTitol());
         System.out.println("Autor: " + getAutor());
